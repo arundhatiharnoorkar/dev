34bb0085d45418f65877efd7a056bb4d5ee85659
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-04-23 12:44:34 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+  public int add(int a,int b){
+    return a+b;
+  }
+   public int subtract(int a,int b){
+    return a-b;
+  }
+   public int multiply(int a,int b){
+    return a*b;
+  }
+  public static void main(String[] args){
+    Calculator c=new Calculator();
+    System.out.println(c.add(5,10));
+    System.out.println(c.add(11,10));
+    System.out.println(c.add(5,10));
+  }
+}
